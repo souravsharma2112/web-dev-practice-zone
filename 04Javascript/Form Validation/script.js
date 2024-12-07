699b9a9d432187ccbc816a3aca6535507b85d2d6
Sourav Kumar <souravsharma2102@gmail.com>
2024-12-07 22:31:25 +0530

    little update in form validation

--- 04Javascript/Form Validation/script.js
+++ 04Javascript/Form Validation/script.js
@@ -6,17 +6,20 @@ const phoneNo2 = document.getElementById("phoneNo2");
 const formMsg = document.getElementById("formMsg");
 const paraTxt = document.getElementsByClassName("paraTxt");
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+const formSubmitMsg = {
+    name : "At least 3 letter are required.",
+    email : "Please enter a valid email address.",
+    phone : "Phone number must be exactly 10 digits.",
+    msg : "Message must be more than 5 letters.",
+}
 const formSubmit = (event)=> {
     event.preventDefault()
     let isValid = true;
-    (firstName.value.length < 3) ?( paraTxt[0].innerText = "At least 3 letter are required.", isValid = false) :paraTxt[0].innerText = "";
-    (lastName.value.length < 3) ? (paraTxt[1].innerText = "At least 3 letter required", isValid = false) :paraTxt[1].innerText = "";
-    (!emailPattern.test(email.value))?(paraTxt[2].innerText = "Please enter a valid email address.", isValid = false) :paraTxt[2].innerText = "";
-    (phoneNo1.value.length < 10) ? (paraTxt[3].innerText = "Phone number must be exactly 10 digits.", isValid = false) :paraTxt[3].innerText = "";
-    (phoneNo2.value.length < 10) ? (paraTxt[4].innerText = "Phone number must be exactly 10 digits.", isValid = false) :paraTxt[4].innerText = "";
-    (formMsg.value.length < 5) ? (paraTxt[5].innerText = "Message must be more than 5 letters.", isValid = false) :paraTxt[5].innerText = "";
-    if(isValid){
-        event.target.submit()
-    }   
+    (firstName.value.length < 3) ?( paraTxt[0].innerText = formSubmitMsg.name , isValid = false) :paraTxt[0].innerText = "";
+    (lastName.value.length < 3) ? (paraTxt[1].innerText = formSubmitMsg.name , isValid = false) :paraTxt[1].innerText = "";
+    (!emailPattern.test(email.value))?(paraTxt[2].innerText = formSubmitMsg.email , isValid = false) :paraTxt[2].innerText = "";
+    (phoneNo1.value.length < 10) ? (paraTxt[3].innerText = formSubmitMsg.phone, isValid = false) :paraTxt[3].innerText = "";
+    (phoneNo2.value.length < 10) ? (paraTxt[4].innerText = formSubmitMsg.phone, isValid = false) :paraTxt[4].innerText = "";
+    (formMsg.value.length < 5) ? (paraTxt[5].innerText = formSubmitMsg.msg , isValid = false) :paraTxt[5].innerText = "";
+    if(isValid) event.target.submit() 
 }
